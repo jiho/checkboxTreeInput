318d5e9fc9b0b0d8d27c98b8c9ffdfd43d0d1b91
Jean-Olivier Irisson <irisson@normalesup.org>
2017-03-04 15:30:29 +0100

    Make the example a comparison between checkboxGroup and checkboxTree

--- inst/examples/01_test.R
+++ inst/examples/01_test.R
@@ -1,25 +1,28 @@
 library("shiny")
 library("checkboxTreeInput")
 
-tree <- data.frame(id=1:6, parent_id=c(NA, 1, 2, 2, 1, NA))
+tree <- data.frame(id=1:6, parent_id=c(NA, 1, 2, 2, NA, 1))
 
 ui <- function(request) {
   fluidPage(
     sidebarLayout(
       sidebarPanel(
-        checkboxTreeInput("taxo", label="Tree selector", tree=tree, selected=c(1, 6), opened=1)
+        checkboxGroupInput("group", label="checkboxGroup selector", choices=1:6, selected=1:3),
+        checkboxTreeInput("tree", label="checkboxTree selector", tree=tree, selected=c(1, 6), opened=1)
        ),
       mainPanel(
-        verbatimTextOutput("selected")
+        h2("checkboxGroup"),
+        verbatimTextOutput("group"),
+        h2("checkboxTree"),
+        verbatimTextOutput("tree")
       )
     )
   )
 }
 
 server <- function(input, output, session) {
-  output$selected <- renderPrint(
-    input$taxo
-  )
+  output$group <- renderPrint(input$group)
+  output$tree <- renderPrint(input$tree)
 }
 
 shinyApp(ui, server)
